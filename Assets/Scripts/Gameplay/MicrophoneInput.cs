d3f95159c1486acf6f6e0c6d34f7fcf73f83db3b
scarlettmichanco <scarlett@vynyl.com>
2016-10-14 12:12:34 -0600

    Microphone input script

--- Assets/Scripts/Gameplay/MicrophoneInput.cs
+++ Assets/Scripts/Gameplay/MicrophoneInput.cs
@@ -9,27 +9,31 @@ public float pitch = 0;
 
 	// Use this for initialization
 	void Start () {
-		audio.clip = Microphone.Start(null, true, 10, 44100);
-		audio.loop = true;
-		audio.mute = true;
-		while (!(Microphone.GetPosition(AudioInputDevice) > 0)){} // wait for recording
+		AudioSource audio = GetComponent<AudioSource>();
+		GetComponent<AudioSource>().clip = Microphone.Start(null, true, 10, 44100);
+		GetComponent<AudioSource>().loop = true;
+		GetComponent<AudioSource>().mute = true;
+		while (!(Microphone.GetPosition(null) > 0)){} // wait for recording
 		audio.Play();
+		audio.Play(44100);
 		
 	}
 
 	float GetVolumeLevel() {
 		float[] data = new float[256];
-		audio.GetOutputData(data, 0);
-		float average = 0;
+		GetComponent<AudioSource>().GetOutputData(data, 0);
+		float avg = 0;
 		foreach(float vSet in data) {
-			average += Mathf.abs(vSet);
+			avg += Mathf.Abs(vSet);
 		}
-		return average/256;
+		return avg/256;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// set pitch equal to average volume level * sensitivity;
 		pitch = GetVolumeLevel() * sensitivity;
+		Debug.Log("pitch");
+		Debug.Log(pitch);
 	}
 }
---
+++ Assets/Scripts/Gameplay/MicrophoneTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicrophoneTrigger : MonoBehaviour {
+
+	public GameObject audioInputObject;
+	public float threshold = 1.0f;
+	public GameObject scareSoundLevel;
+	MicrophoneInput micIn;
+	// Use this for initialization
+	void Start () {
+		micIn = (MicrophoneInput) audioInputObject.GetComponent("MicrophoneInput");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float L = micIn.pitch;
+		if (L > threshold) {
+			Debug.Log("inputLevel");
+			Debug.Log(threshold);
+		}
+	}
+}
