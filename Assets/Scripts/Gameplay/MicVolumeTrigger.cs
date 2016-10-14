e887d17556080a940c806b51a5ed82c89736a7e9
jeremy <jeremy@weareenvoy.com>
2016-10-14 13:51:06 -0600

    getting mic volume average

---
+++ Assets/Scripts/Gameplay/MicVolumeTrigger.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicVolumeTrigger : MonoBehaviour {
+
+	public float Threshold = 100f;
+
+	private AudioSource audioSource;
+	private int _sampleWindow = 128;
+
+	private string chosenDevice;
+
+	// Use this for initialization
+	void Start () {
+
+		foreach (string device in Microphone.devices) {
+            Debug.Log("Name: " + device);
+        }
+
+		//deviceName = "Built-in Microphone";
+
+		audioSource = GetComponent<AudioSource>();
+		chosenDevice = "Built-in Microphone";
+		StartMicrophone(chosenDevice);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (LevelMax() >= 100)
+		{
+			Debug.Log("Threshold met!");
+			GameManager.Instance.NoiseEventTriggered();
+		}		
+	}
+
+	private float LevelMax()
+    {
+        float levelMax = 0;
+        float[] waveData = new float[_sampleWindow];
+        int micPosition = Microphone.GetPosition (null) - (_sampleWindow + 1);
+        if (micPosition < 0) {
+            return 0;
+        }
+        audioSource.clip.GetData (waveData, micPosition);
+        for (int i = 0; i < _sampleWindow; ++i) {
+            float wavePeak = waveData [i] * waveData [i];
+            if (levelMax < wavePeak) {
+                levelMax = wavePeak;
+            }
+        }
+        return levelMax * 1000;
+    }
+
+	public void StartMicrophone(string deviceName)
+	{
+		audioSource.clip = Microphone.Start(deviceName, true, 5, 44100);
+	}
+
+	public void StopMicrophone(string deviceName)
+	{
+		Microphone.End(deviceName);
+	}
+
+	void OnDestroy()
+	{
+		StopMicrophone(chosenDevice);
+	}
+}
