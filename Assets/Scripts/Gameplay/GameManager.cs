3855da91f69f86dcec1453d00a54fd757932d49d
jeremy <jeremy@weareenvoy.com>
2016-10-14 10:47:48 -0600

    added init game manager and points on collision

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -3,14 +3,37 @@ using System.Collections;
 
 public class GameManager : GenericSingletonClass<GameManager> {
 
+	private bool RoundStarted = false;
+
+	private int totalPoints = 0;
+
 	// Use this for initialization
 	void Start()
 	{
 
 	}
+
+	public void StartRound()
+	{
+		RoundStarted = true;
+	}
+
+	public void EndRound()
+	{
+		//display gameover card,
+
+		//
+		RoundStarted = false;
+	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void AddPoints(int points)
+	{
+		Debug.Log("Added points: " + points);
+		totalPoints += points;
 	}
 }
---
+++ Assets/Scripts/Gameplay/PointsOnCollision.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PointsOnCollision : MonoBehaviour {
+	[Tooltip("Number of points to award during collision")]
+	public int NumberOfPoints;
+
+	[Tooltip("Multiplier for force of impact (set to 0 for no difference)")]
+	public float ForceMultiplier = 0.0f;
+
+	private bool hasCollided = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter (Collision col)
+    {
+		if (!hasCollided)
+		{
+			GameManager.Instance.AddPoints(NumberOfPoints);
+		}
+
+        hasCollided = true;
+    }
+}
