bd4205336187e0798e797ab1435a2205ef2f3239
scarlettmichanco <scarlett@vynyl.com>
2016-10-14 11:15:56 -0600

    microphone trigger input

---
+++ Assets/Scripts/Gameplay/MicrophoneTrigger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneTrigger : MonoBehaviour {
+
+public float sensitivity = 100;
+public float pitch = 0;
+
+	// Use this for initialization
+	void Start () {
+		audio.clip = Microphone.Start(null, true, 10, 44100);
+		audio.loop = true;
+		audio.mute = true;
+		while (!(Microphone.GetPosition(AudioInputDevice) > 0)){} // wait for recording
+		audio.Play();
+		
+	}
+
+	float GetVolumeLevel() {
+		float[] data = new float[256];
+		audio.GetOutputData(data, 0);
+		float average = 0;
+		foreach(float vSet in data) {
+			average += Mathf.abs(vSet);
+		}
+		return average/256;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// set pitch equal to average volume level * sensitivity;
+		pitch = GetVolumeLevel() * sensitivity;
+	}
+}
