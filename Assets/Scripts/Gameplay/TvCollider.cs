f43cac5eca4c8a1a3aaf66141b2035a258694028
Jordan Clifford <jordan@jfclifford.com>
2016-10-15 12:13:39 -0600

    added tv script

---
+++ Assets/Scripts/Gameplay/TvCollider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TvCollider : MonoBehaviour {
+
+	public GameObject tvStatic;
+	public GameObject tvLight;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		tvStatic.SetActive (true);
+		tvLight.SetActive (true);
+	}
+
+}
