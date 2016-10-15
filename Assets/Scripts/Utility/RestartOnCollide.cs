7d40283dfe2a23573fae54902a3bd79e99895fe0
scarlettmichanco <scarlett@vynyl.com>
2016-10-15 13:35:51 -0600

    fix file

--- Assets/Scripts/Utility/RestartOnCollide.cs
+++ Assets/Scripts/Utility/RestartOnCollide.cs
@@ -5,12 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class RestartOnCollide : MonoBehaviour {
 
-	void Start() {
-		
-	}
-
 	void OnTriggerEnter(Collider col)
 	{
-	  SceneManager.LoadScene ("Bedroom");
+
+		SceneManager.LoadScene ("Bedroom");
+		//GameManager.Instance.StartRound();
+
 	}
 }
