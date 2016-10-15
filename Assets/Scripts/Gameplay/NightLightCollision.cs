337d3e9f3e5927fe071e9f61c67eb1edf0786a67
jeremy <jeremy@weareenvoy.com>
2016-10-14 18:45:49 -0600

    nightlight action

---
+++ Assets/Scripts/Gameplay/NightLightCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NightLightCollision : MonoBehaviour {
+
+	public Light light;
+
+	void OnCollisionEnter(Collision col)
+	{
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.isKinematic = false;
+		rb.useGravity = true;
+		light.enabled = false;
+	}
+}
