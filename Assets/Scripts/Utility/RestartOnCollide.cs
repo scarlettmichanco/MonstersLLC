18713308d07434b83e6736e08d35411ca8b8bbeb
scarlettmichanco <scarlett@vynyl.com>
2016-10-15 13:20:51 -0600

    change start script

--- Assets/Scripts/Utility/RestartOnCollide.cs
+++ Assets/Scripts/Utility/RestartOnCollide.cs
@@ -5,11 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class RestartOnCollide : MonoBehaviour {
 
+	void Start() {
+		
+	}
+
 	void OnTriggerEnter(Collider col)
 	{
-
-		SceneManager.LoadScene ("Bedroom");
-		//GameManager.Instance.StartRound();
-
+	  SceneManager.LoadScene ("Bedroom");
 	}
 }
