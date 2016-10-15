991f5df3990088113cc3c18d2f34d66a49c7f7f0
jeremy <jeremy@weareenvoy.com>
2016-10-15 13:17:31 -0600

    added TV stuff

--- Assets/Scripts/Gameplay/TvCollider.cs
+++ Assets/Scripts/Gameplay/TvCollider.cs
@@ -5,19 +5,32 @@ public class TvCollider : MonoBehaviour {
 
 	public GameObject tvStatic;
 	public GameObject tvLight;
+	public Material screenMaterial;
+	public AudioSource staticSound;
+
+	private bool turnedOn = false;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!turnedOn) {
+			return;
+		}
+		screenMaterial.mainTextureOffset = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
 	}
 	void OnTriggerEnter(Collider col)
 	{
 		tvStatic.SetActive (true);
 		tvLight.SetActive (true);
+
+		if (!staticSound.isPlaying)
+		{
+			staticSound.Play();
+		}
+		turnedOn = true;
 	}
 
 }
